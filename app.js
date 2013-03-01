07decde30910746fa105d8c396b9889e02098bb7
jeliasrm <jesus.eliot.rm@gmail.com>
2013-02-28 21:53:11 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'),http = require('http');
+var app = express();
+var server = http.createServer(app);
+app.set('views',__dirname + '/views');
+app.configure(function(){
+  app.use(express.static(__dirname));
+});
+app.get('/',function(req,res){
+	res.render('index.jade',{layout:false});
+});
+server.listen(8000);
+
+//websockets
+
+var io = require('socket.io').listen(server);
+var usuariosConectados = {};
+io.sockets.on('connection',function(socket){
+	
+});
